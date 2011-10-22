42c30ba7de096fb94627a29611fd189ae5abc8fd
Pepe Doval <pepellou@gmail.com>
2011-10-22 09:46:39 +0200

    Refactor

--- test/ArrayListTest.php
+++ test/ArrayListTest.php
@@ -6,9 +6,7 @@ class ArrayListTest extends PHPUnit_Framework_TestCase {
 	
 	public function test_removeLast(
 	) {
-		$values = new ArrayList(array(1, 2, 3, 4, 5));
-		$this->assertEquals(5, $values->removeLast());
-		$this->assertEquals(4, $values->removeLast());
+		$values = new ArrayList(array(1, 2, 3));
 		$this->assertEquals(3, $values->removeLast());
 		$this->assertEquals(2, $values->removeLast());
 		$this->assertEquals(1, $values->removeLast());
